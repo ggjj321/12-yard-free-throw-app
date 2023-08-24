01283028552c3a0a24aa872c0fdf12ad751b5e06
ggjj321 <10370324lsjh@gmail.com>
2023-08-24 14:18:04 +0800

    Feat : order by timestamp

--- lib/page/result_display_page.dart
+++ lib/page/result_display_page.dart
@@ -23,7 +23,7 @@ class ResultDisplayPage extends StatelessWidget {
     return Scaffold(
       appBar: waveAppbar("Result Page"),
       body: FutureBuilder<QuerySnapshot>(
-        future: users.get(),
+        future: users.orderBy('created_at', descending: true).get(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
             return const Text("Something went wrong");
